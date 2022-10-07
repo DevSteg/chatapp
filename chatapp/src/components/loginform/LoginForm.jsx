e38589c7d66c9c2ca2351f46694e4a0d07610e08
James Stephens <devstegg@gmail.com>
2022-10-07 20:11:38 +0100

    Add: Login functionality

--- chatapp/src/components/loginform/LoginForm.jsx
+++ chatapp/src/components/loginform/LoginForm.jsx
@@ -1,20 +1,64 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
+import { useState } from "react";
+import { loginRequest } from "../../api/loginRequest";
 
 const LoginForm = () => {
+	const [loginData, setLoginData] = useState({
+		email: "",
+		password: "",
+	});
+
+	const handleOnSubmit = async (e) => {
+		e.preventDefault();
+		// Check user has completed input fields
+		if (!loginData.email || !loginData.password) {
+			alert("Please enter Email Address and Password");
+		}
+		// call POST request to backend
+		const loginSuccess = await loginRequest(loginData);
+		if (loginSuccess) {
+			console.log("Login Successful");
+		}
+		location.reload(); // Reload page
+	};
+
+	// Update the state with user input
+	const handleOnChange = (e) => {
+		const { value, name } = e.target;
+		setLoginData({
+			...loginData,
+			[name]: value,
+		});
+	};
+
 	return (
 		<>
-			<Form>
+			<Form onSubmit={handleOnSubmit}>
 				<Container>
-					<Form.Group controlId="login-form">
+					<Form.Group controlId="email">
 						<Form.Label className="form-label">Email Address</Form.Label>
-						<Form.Control type="email" placeholder="Enter Email" />
+						<Form.Control
+							type="email"
+							placeholder="Enter Email"
+							name="email"
+							value={loginData.email}
+							onChange={handleOnChange}
+							required
+						/>
 						<Form.Text>We'll never share your email with anyone else</Form.Text>
 					</Form.Group>
-					<Form.Group controlId="login-form">
+					<Form.Group controlId="password">
 						<Form.Label className="form-label">Password</Form.Label>
-						<Form.Control type="password" placeholder="Enter Password" />
+						<Form.Control
+							type="password"
+							placeholder="Enter Password"
+							name="password"
+							value={loginData.password}
+							onChange={handleOnChange}
+							required
+						/>
 					</Form.Group>
 					<Button
 						className="login-submit"
--- chatapp/src/pages/login/Login.jsx
+++ chatapp/src/pages/login/Login.jsx
@@ -1,9 +1,14 @@
 import LoginForm from "../../components/loginform/LoginForm";
 import "./_login.scss";
+import { Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/context";
 
 const Login = () => {
+	const { token } = useContext(AuthContext);
 	return (
 		<section className="login-page">
+			{token && <Navigate to="/profile" />}
 			<h1 className="login-title">Login</h1>
 			<div className="login-form">
 				<LoginForm />
