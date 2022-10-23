8f28623117220135fe383d1001a558ebacdcdf2c
James Stephens <devstegg@gmail.com>
2022-10-23 22:34:16 +0100

    Refactor: relocate the scss import

--- chatapp/src/main.jsx
+++ chatapp/src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/main.scss";
 import { AuthContextProvider } from "./context/context";
 import { UserContextProvider } from "./context/userContext";
+import "./styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
